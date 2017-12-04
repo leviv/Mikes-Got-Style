a406c63ba53c8b154b1b2bc4da21db6a90ddc88c
Levi V <villarreallevi@gmail.com>
2017-12-04 12:41:39 -0600

    revert the revert

--- popup.js
+++ popup.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 function toggle() {
     chrome.storage.sync.get('isPretty', function(data){
         if(data.isPretty == true){
             chrome.storage.sync.set({'isPretty': false}, function(){
                 button.style.fill = "red";
                 chrome.tabs.executeScript({
-                    code: "var body = document.getElementsByTagName(\"body\")[0]; body.className = \"\";"
+                    code: "var body = document.getElementsByTagName(\"body\")[0]; body.className = \"\""
                 });
             });
         }else{
@@ -18,31 +17,25 @@ function toggle() {
         }
     });
     
-=======
-
-function hello() {
-  chrome.tabs.executeScript({
-    file: 'toggle.js'
-  }); 
->>>>>>> parent of 00bb3b0... Now the current state is saved in local storage
 }
 
 window.onload = function(){
     var button = document.getElementById('button');
-    button.style.fill = "red";
+    init();
     button.onmousedown = handleClick;
 };
 
 function handleClick(evt){
     evt.preventDefault();
-    hello();
-    toggleColor();
+    toggle();
 }
 
-function toggleColor(){
-    if(button.style.fill == "red"){
-        button.style.fill = "green";
-    }else{
-        button.style.fill = "red";
-    }
+function init(){
+    chrome.storage.sync.get('isPretty', function(data){
+        if(data.isPretty == true){
+            button.style.fill = "green";
+        }else{
+            button.style.fill = "red";
+        }
+    });
 }
